7e233df2f88cc2f72dde9905a397d7bd1f3fab6e
Tatiana Rubio <tatiana789rubio@gmail.com>
2025-09-16 20:04:57 -0500

    mapeando DTO

--- src/main/java/com/example/API/MODELO/MIERCOLES/modelos/dtos/mapas/IMapaNotaDTO.java
+++ src/main/java/com/example/API/MODELO/MIERCOLES/modelos/dtos/mapas/IMapaNotaDTO.java
@@ -3,7 +3,10 @@ package com.example.API.MODELO.MIERCOLES.modelos.dtos.mapas;
 import com.example.API.MODELO.MIERCOLES.modelos.Nota;
 import com.example.API.MODELO.MIERCOLES.modelos.dtos.NotaDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface IMapaNotaDTO {
@@ -12,5 +15,12 @@ public interface IMapaNotaDTO {
 
     //Convirtiendo modelodto en DTO
 
+    @Mapping(source= "valor", target = "valor")
+    @Mapping(source= "tipoEvaluacion", target = "tipoEvaluacion")
+    @Mapping(source= "fecha", target = "fecha")
+    @Mapping(source= "materia.materia", target = "materia")
     NotaDTO convertirADTO(Nota nota);
+
+    //Mapeando una lista de datos de modelo a una lista de datos DTO
+    List<NotaDTO>convertir_lista_a_DTO(List<Nota>lista);
 }
